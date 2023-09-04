bb5dbaed0a92f7a374930f609fb962b2507b7d73
TimLiu <timliudream@163.com>
2023-09-04 21:59:28 +0800

    :sparkles: goal modal

--- src/views/dashboard/analysis/components/GrowCard.vue
+++ src/views/dashboard/analysis/components/GrowCard.vue
@@ -24,7 +24,14 @@
           :endVal="FireData.total"
           class="text-2xl"
         />
-        <!--        <a-button type="primary" @click="send"> 更改目标 </a-button>-->
+
+        <a-button type="primary" @click="update_goal"> 更改目标 </a-button>
+        <!--        <component-->
+        <!--          :is="currentModal"-->
+        <!--          v-model:visible="modalVisible"-->
+        <!--          :userData="userData"-->
+        <!--        />-->
+        <GoalModal @register="goal" />
       </div>
     </Card>
   </div>
@@ -32,13 +39,16 @@
 <script lang="ts" setup>
 import { CountTo } from "/@/components/CountTo/index";
 import Icon from "@/components/Icon/Icon.vue";
-import { Tag, Card } from "ant-design-vue";
-import { onMounted, reactive } from "vue";
+import { Card } from "ant-design-vue";
+import { ComponentOptions, onMounted, reactive, ref, shallowRef } from "vue";
 import { useMessage } from "@/hooks/web/useMessage";
 import { useI18n } from "@/hooks/web/useI18n";
 import { useDesign } from "@/hooks/web/useDesign";
 import { useFamilyStore } from "@/store/modules/family";
 import { useUserStore } from "@/store/modules/user";
+import { useModal } from "@/components/Modal";
+import GoalModal from "@/views/dashboard/analysis/components/GoalModal.vue";
+// import BasicModal from "@/components/Modal/src/BasicModal.vue";
 
 const { prefixCls } = useDesign("login");
 const { t } = useI18n();
@@ -88,5 +98,18 @@ async function fetchFireGoalData() {
         document.body.querySelector(`.${prefixCls}`) || document.body,
     });
   }
+}
+
+const currentModal = shallowRef<Nullable<ComponentOptions>>(null);
+const [goal, { openModal: openGoal }] = useModal();
+const modalVisible = ref<Boolean>(false);
+const userData = ref<any>(null);
+
+function update_goal() {
+  openGoal(true, {
+    data: "content",
+    info: "Info",
+    title: "update",
+  });
 }
 </script>
